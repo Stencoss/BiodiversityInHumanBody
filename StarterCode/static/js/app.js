aa89419d53da5226113778c7478d29b4fede5178
StevenNimble <stadenoia@gmail.com>
2022-04-03 12:31:26 -0500

    Starter code and loading data

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,9 @@
+// Part 1
+// Use D3 Libary to read in samples - refer to previous code from porject
+// TODO: Plotly - horizontal bar chart with a dropdown menu - needs to display the top 10 OTU
+// TODO:Labels - all taken from sample dataset
+// TODO:Values = sample_values
+// TODO:otu_ids = ;abels for bar chart
+// TODO:otu_labels = hovertext for chart 
+
+d3.json('samples.json').then(function(data){ console.log(data)});
