f75d5240f84b8a540fc598d9fc222ab35e57d205
StevenNimble <stadenoia@gmail.com>
2022-04-04 08:36:37 -0500

    comments and fine tuning demo

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -31,21 +31,25 @@ function dropDown(sdata) {
 
 }
 // Try to pull the data from the drop down menu
-function optionChanged(selectedObject) {
-    var value = selectedObject
-    console.log(value);
-    let data = d3.json("samples.json").then(function(d){
-        // console.log(d.metadata.length)
+// The next three functions can probably be combined to be less clucky
+// TODO: Combine dropdown and demographics functions
+function optionChanged(selectedObject) {        // references the function in index.html
+    var value = selectedObject                  // Unneeded and can probably just pass it
+    console.log(value);                         // Checked if it is the correct value
+    let data = d3.json("samples.json").then(function(d){    // Pulling JSON
+        // console.log(d.metadata.length)                   // Getting array length to pass
         let len = d.metadata.length
-        demo_search(value, len);
-    });
+        demo_search(value, len);                            // Pass to the search function
+    }); 
 }
 
-
+// Takes in data which is the meta data.  
+//  Seearch is the index of the  array of the name selected
+//  Creates a unordered list of demographics
 function demographics(data, search) {
-    var select_demo = d3.select('#sample-metadata');
-    select_demo.text("New Data")
-    var demoOut = select_demo.append("ul"); 
+    var select_demo = d3.select('#sample-metadata');    // Selects the html class to put list in
+    select_demo.text("New Data")                        // Helps to delete stale data so new one is inputted
+    var demoOut = select_demo.append("ul");             // Creates list follow by the list
     var id = demoOut.append("li").text("ID: " + data[search].id);
     var bb = demoOut.append("li").text("BBtype: " + data[search].bbtype);
     var eth = demoOut.append("li").text("Ethnicity: " + data[search].ethnicity);
@@ -56,16 +60,18 @@ function demographics(data, search) {
 
 }
 
+// Searches through the array / JSON (metadata) to find the patient ID from optionCHanged
+// Search is the patient ID/name and len is the length of the array to search
 function demo_search(search, len){
-    let data = d3.json("samples.json").then(function(d){
+    let data = d3.json("samples.json").then(function(d){    // Load data
         // console.log(d.metadata.length)
-        let meta = d.metadata
-        console.log("My search: " + search)
-        for ( var i = 0; i < len; i++){
-            if (meta[i].id == search){
+        let meta = d.metadata                               // Just need meta data
+        console.log("My search: " + search)                 // Test on what I am searching for
+        for ( var i = 0; i < len; i++){                     // For loop to find the name
+            if (meta[i].id == search){                      // Did I find it?
                 console.log("I found you!!!" + search)
-                demographics(meta, i)
-            }
+                demographics(meta, i)                       // Pass to demographics which populates the field
+            }                                               // meta is the data and i is the index of the name
         }
     });
 
